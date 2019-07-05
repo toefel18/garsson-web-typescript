ba4a8d9ba53b650591f97b2817d75a0be89aa3be
toefel <toefel18@gmail.com>
2019-07-05 23:03:57 +0200

    Added websocket listener

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,25 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import AppState from "./AppState";
 
+const updateStream = new WebSocket("ws://localhost:8080/api/v1/update-stream");
+
+updateStream.onopen = function (event) {
+    console.log("Websocket opened", event)
+}
+
+updateStream.onmessage = function (event) {
+    const message = JSON.parse(event.data)
+    console.log("Websocket message received ",message, event)
+    alert("Websocket msg received" + message)
+}
+
+updateStream.onerror = function(error) {
+    console.log("Websocket error", error)
+    alert("Websocket error " + error)
+}
+
+
+
 ReactDOM.render(<AppState />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
